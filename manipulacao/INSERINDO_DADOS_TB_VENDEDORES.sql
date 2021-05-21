723d3ca694d34abe567f0940fcc1cb5e44e679a4
adenilson1 <adenilson99@hotmail.com>
2021-05-20 21:25:39 -0300

    SQL de inserir dados na tabela vendedores

---
+++ manipulacao/INSERINDO_DADOS_TB_VENDEDORES.sql
@@ -0,0 +1,14 @@
+DESC tb_vendedores
+
+INSERT INTO tb_vendedores (matricula, nome, percentual_comissao)
+VALUES
+('00233', 'José Geraldo da Fonseca', 0.1)
+
+SELECT * FROM TB_VENDEDORES
+
+INSERT INTO tb_vendedores (matricula, nome, percentual_comissao)
+VALUES
+('00235', 'Márcio Almeida Silva', 0.08);
+INSERT INTO tb_vendedores (matricula, nome, percentual_comissao)
+VALUES
+('00236', 'Cláudia Morais', 0.08);
