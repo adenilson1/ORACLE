fd92750513a95e72904c6f8b20c0d2d15e9175b9
adenilson1 <adenilson99@hotmail.com>
2021-05-21 20:27:31 -0300

    SQL de filtros, uso do WHERE

---
+++ manipulacao/FILTROS.sql
@@ -0,0 +1,20 @@
+SELECT * FROM TB_PRODUTOS
+
+SELECT * FROM TB_PRODUTOS
+WHERE PRODUTO = '1037797'
+
+SELECT * FROM TB_CLIENTES 
+WHERE CIDADE = 'Rio de Janeiro'
+
+select * from tb_produtos
+where sabor = 'Laranja'
+
+select * from tb_produtos
+where sabor = 'Limão'
+
+UPDATE TB_PRODUTOS SET SABOR = 'Citricos'
+WHERE SABOR = 'Limão'
+
+SELECT * FROM TB_PRODUTOS
+WHERE SABOR = 'Citricos'
+
